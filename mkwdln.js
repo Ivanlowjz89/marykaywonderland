ad51222df4505df880c7ca2e257c7c69efeaad03
wearegladstone <81361622+wearegladstone@users.noreply.github.com>
2021-04-08 13:30:19 +0800

    Update mkwdln.js

--- mkwdln.js
+++ mkwdln.js
@@ -237,7 +237,7 @@ document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
-$('#menuBlock').css("width", $('#menuBlock').height() * 1.898);
+//$('#menuBlock').css("width", $('#menuBlock').height() * 1.898);
 });
 
 document.addEventListener("visibilitychange", function() {
